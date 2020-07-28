a1f931c1b61255ea4e68464088e6634c7bd5916e
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2020-07-28 18:45:52 +0530

    Add bat, dunst, compton, xserver and vscode config

---
+++ dunst/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Create symlinks
+echo "Creating dunst symlinks..."
+ln -s ~/.dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
+echo "Done!"
--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,4 +1,4 @@
 ############################# FZF Variables ############################
 
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --theme='OneHalfDark' --color=always {}' --layout=reverse --cycle --preview-window='right:70%:wrap' -x --margin=1,4 --height=75%"
+export FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always {}' --layout=reverse --cycle --border --preview-window='right:70%:wrap' -x --margin=1,4 --height=75%"
