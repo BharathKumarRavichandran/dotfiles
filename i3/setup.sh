4b0f2abb373abd5c82d84a459974f694aa59a45b
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2020-12-25 04:20:07 +0530

    Fri Dec 25 04:20:00 IST 2020
    * Add pacman and python packages list
    * Add media player controls to i3 config
    * Switch to PulseAudio from ALSA
        * Add script for volume control
    * Update compton config
    * Change termite transparency level
    * Change dunst color scheme
    * Switch to Iosevka
    * Update VSCode settings

--- i3/setup.sh
+++ i3/setup.sh
@@ -2,6 +2,7 @@
 
 # Create symlinks
 echo "Creating i3 symlinks..."
+mkdir -p ~/.config/i3status
 ln -s ~/.dotfiles/i3/status_config ~/.config/i3status/config
 ln -s ~/.dotfiles/i3/config ~/.i3/config
 echo "Done!"
